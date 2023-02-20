f1f183daa3ec2d809b10fc4208cb87e7acb1a121
Prashant Bhuruk <prashantbhuruk88@gmail.com>
2023-02-19 16:43:01 -0800

    Add Error handling

--- hello/hello.go
+++ hello/hello.go
@@ -5,12 +5,21 @@ import (
 
 	"rsc.io/quote"
 
+	"log"
+
 	"github.com/CoderPraBhu/go-programs/greetings"
 )
 
 func main() {
+
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0) // time, source, line
 	fmt.Println("Hello, World!")
 	fmt.Println(quote.Go())
-	message := greetings.Hello("Prashant")
+	message, err := greetings.Hello("")
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	fmt.Println(message)
 }
